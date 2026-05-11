a916ef5f362af8a5bb1246e444d95ddda3fdcb4d
billy <billyjoelballola27@gmail.com>
2026-05-11 13:40:31 +0800

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -11,9 +11,11 @@ const nextConfig: NextConfig = {
             value: [
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com",
+              "script-src-elem 'self' 'unsafe-inline' https://challenges.cloudflare.com",
               "frame-src 'self' https://challenges.cloudflare.com",
               "style-src 'self' 'unsafe-inline'",
               "connect-src 'self' https://challenges.cloudflare.com",
+              "worker-src blob: https://challenges.cloudflare.com",
             ].join("; "),
           },
         ],
